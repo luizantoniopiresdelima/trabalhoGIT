468f5959684b2cfaf4ac583fe4f9f7f99435785a
Luiz <luiztrace2003@gmail.com>
2024-06-30 22:18:25 -0300

    criacao do metodo de somar

---
+++ src/main/java/com/mycompany/trabalho_git/operacoes.java
@@ -0,0 +1,14 @@
+package com.mycompany.trabalho_git;
+public class operacoes {
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int soma = somar(5, 3);
+        System.out.println("Soma: " + soma); 
+
+    }
+} 
+
